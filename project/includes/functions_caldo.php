d5138a7108cd19f79e10341e5499fbba6edc4e52
santiago1991 <santiago1991@localhost>
2010-06-15 19:24:26 -0500

    Se a?ade el atributo numero de votos a las conferencias. Se inicia el desarrollo de la interfaz gr?fica durante la creaci?n del CALDO en la creaci?n de POSTS. Se inicia el desarrollo y se describen las algunas acciones en function_caldo.php

--- project/includes/functions_caldo.php
+++ project/includes/functions_caldo.php
@@ -1,2 +1,22 @@
 <?php
+/*! @function submit_caldo
+    @abstract Crea un caldo para una conferencia en un foro específico 
+    @param forum_id int - El id del foro en el que se desea crear el caldo
+    @param criterios array - Un array de los id's de los criterios que incluye el caldo 
+    @param conferencia array - Un array compuesto por 'titulo' y 'fecha', que da información sobre la conferencia a la que hará referencia el caldo
+    @param Votantes array - Un array de los id's de los nombres de usuarios habilitados para votar
+    @param conferencistas array - Un array con los nombres de usuarios que actuaron como conferencisatas y que portanto pueden ver el resultado de sus caldos.
+    @result Boolean - Devuelve verdadero si la creación del caldo fue exitosa y falso en cualquier otro caso.
+ */
+function submit_caldo($forum_id,$criterios,$conferencia,$votantes,$conferencistas){
+	//Verifique que exista el foro identificado por $forum_id
+	
+	//Verifique que existan los criterios identificados por los id's en el array $criterios
+	
+	//Cree la conferencia y almacene su id en la variable $conferencia_id
+	
+	//Verifique la existencia de los nombres de usuarios en el array votantes y en el array conferencistas
+	
+	//
+}
 ?>
