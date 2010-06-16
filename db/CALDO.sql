2c9bc9dfa62b8fd50860fd7492eb4529f668b161
santiago1991 <santiago1991@localhost>
2010-06-15 20:55:39 -0500

    Se pasan todas las tablas a MyIsam que es el que maneja phpbb y a utf8_bin Se deja el script de generaci?n de la base de datos funcionando (Probado). Algunos avances en submit_caldo

--- db/CALDO.sql
+++ db/CALDO.sql
@@ -1,48 +1,64 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-CREATE SCHEMA IF NOT EXISTS `phpbb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`tokens`
+-- Table `phpbb`.`caldo_tokens`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`tokens` (
+DROP TABLE IF EXISTS `phpbb`.`caldo_tokens` ;
+
+CREATE  TABLE IF NOT EXISTS `phpbb`.`caldo_tokens` (
   `token` VARCHAR(10) NOT NULL ,
-  `asignado` TINYINT(1)  NOT NULL ,
-  `fecha_vencimiento` TIMESTAMP NOT NULL ,
+  `asignado` TINYINT(1)  NOT NULL COMMENT 'Indica si el token ya ha sido asignado para que no pueda ser usado una vez mas' ,
+  `fecha_vencimiento` TIMESTAMP NOT NULL COMMENT 'Fecha de vencimiento para que no queden almacenados por siempe los tokens que se generen' ,
   PRIMARY KEY (`token`) )
-ENGINE = InnoDB;
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`conferencia`
+-- Table `phpbb`.`conferencia`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`conferencia` (
+DROP TABLE IF EXISTS `phpbb`.`conferencia` ;
+
+CREATE  TABLE IF NOT EXISTS `phpbb`.`conferencia` (
   `id_conferencia` INT NOT NULL AUTO_INCREMENT ,
   `fecha_realizacion` DATE NOT NULL ,
   `tema` VARCHAR(50) NOT NULL ,
-  PRIMARY KEY (`id_conferencia`) )
-ENGINE = InnoDB;
+  `numero_de_votos` INT NOT NULL DEFAULT 0 COMMENT 'Número de votos realizados para calcular las puntuaciones de cada criterio' ,
+  `phpbb_forums_forum_id` MEDIUMINT(8) UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id_conferencia`, `phpbb_forums_forum_id`) ,
+  INDEX `fk_conferencia_phpbb_forums1` (`phpbb_forums_forum_id` ASC) ,
+  CONSTRAINT `fk_conferencia_phpbb_forums1`
+    FOREIGN KEY (`phpbb_forums_forum_id` )
+    REFERENCES `phpbb`.`phpbb_forums` (`forum_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`criterios_evaluacion`
+-- Table `phpbb`.`criterios_evaluacion`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`criterios_evaluacion` (
+DROP TABLE IF EXISTS `phpbb`.`criterios_evaluacion` ;
+
+CREATE  TABLE IF NOT EXISTS `phpbb`.`criterios_evaluacion` (
   `id_criterios_evaluacion` INT NOT NULL AUTO_INCREMENT ,
   `nombre` VARCHAR(45) NOT NULL ,
   `descripcion` VARCHAR(45) NULL ,
   PRIMARY KEY (`id_criterios_evaluacion`) )
-ENGINE = InnoDB;
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`criterios_evaluacion_has_conferencia`
+-- Table `phpbb`.`criterios_evaluacion_has_conferencia`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`criterios_evaluacion_has_conferencia` (
+DROP TABLE IF EXISTS `phpbb`.`criterios_evaluacion_has_conferencia` ;
+
+CREATE  TABLE IF NOT EXISTS `phpbb`.`criterios_evaluacion_has_conferencia` (
   `criterios_evaluacion_idcriterios_de_evaluacion` INT NOT NULL ,
   `conferencia_id_conferencia` INT NOT NULL ,
   `puntuacion` INT NOT NULL ,
@@ -51,105 +67,15 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`criterios_evaluacion_has_conferencia` (
   INDEX `fk_criterios_de_evaluacion_has_Conferencia_Conferencia1` (`conferencia_id_conferencia` ASC) ,
   CONSTRAINT `fk_criterios_de_evaluacion_has_Conferencia_criterios_de_evalu`
     FOREIGN KEY (`criterios_evaluacion_idcriterios_de_evaluacion` )
-    REFERENCES `mydb`.`criterios_evaluacion` (`id_criterios_evaluacion` )
+    REFERENCES `phpbb`.`criterios_evaluacion` (`id_criterios_evaluacion` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_criterios_de_evaluacion_has_Conferencia_Conferencia1`
     FOREIGN KEY (`conferencia_id_conferencia` )
-    REFERENCES `mydb`.`conferencia` (`id_conferencia` )
+    REFERENCES `phpbb`.`conferencia` (`id_conferencia` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-USE `phpbb` ;
-
--- -----------------------------------------------------
--- Table `phpbb`.`phpbb_users`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `phpbb`.`phpbb_users` (
-  `user_id` MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `user_type` TINYINT(2) NOT NULL DEFAULT '0' ,
-  `group_id` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '3' ,
-  `user_permissions` MEDIUMTEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `user_perm_from` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_regdate` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `username` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `username_clean` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_password` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_passchg` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_pass_convert` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_email_hash` BIGINT(20) NOT NULL DEFAULT '0' ,
-  `user_birthday` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_lastvisit` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_lastmark` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_lastpost_time` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_lastpage` VARCHAR(200) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_last_confirm_key` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_last_search` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_warnings` TINYINT(4) NOT NULL DEFAULT '0' ,
-  `user_last_warning` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_login_attempts` TINYINT(4) NOT NULL DEFAULT '0' ,
-  `user_inactive_reason` TINYINT(2) NOT NULL DEFAULT '0' ,
-  `user_inactive_time` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_posts` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_lang` VARCHAR(30) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_timezone` DECIMAL(5,2) NOT NULL DEFAULT '0.00' ,
-  `user_dst` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_dateformat` VARCHAR(30) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'd M Y H:i' ,
-  `user_style` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_rank` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_colour` VARCHAR(6) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_new_privmsg` INT(4) NOT NULL DEFAULT '0' ,
-  `user_unread_privmsg` INT(4) NOT NULL DEFAULT '0' ,
-  `user_last_privmsg` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_message_rules` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_full_folder` INT(11) NOT NULL DEFAULT '-3' ,
-  `user_emailtime` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_topic_show_days` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_topic_sortby_type` VARCHAR(1) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 't' ,
-  `user_topic_sortby_dir` VARCHAR(1) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'd' ,
-  `user_post_show_days` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_post_sortby_type` VARCHAR(1) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 't' ,
-  `user_post_sortby_dir` VARCHAR(1) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT 'a' ,
-  `user_notify` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_notify_pm` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_notify_type` TINYINT(4) NOT NULL DEFAULT '0' ,
-  `user_allow_pm` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_allow_viewonline` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_allow_viewemail` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_allow_massemail` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_options` INT(11) UNSIGNED NOT NULL DEFAULT '230271' ,
-  `user_avatar` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_avatar_type` TINYINT(2) NOT NULL DEFAULT '0' ,
-  `user_avatar_width` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_avatar_height` SMALLINT(4) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_sig` MEDIUMTEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `user_sig_bbcode_uid` VARCHAR(8) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_sig_bbcode_bitfield` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_from` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_icq` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_aim` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_yim` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_msnm` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_jabber` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_website` VARCHAR(200) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_occ` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `user_interests` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `user_actkey` VARCHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_newpasswd` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_form_salt` VARCHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '' ,
-  `user_new` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `user_reminded` TINYINT(4) NOT NULL DEFAULT '0' ,
-  `user_reminded_time` INT(11) UNSIGNED NOT NULL DEFAULT '0' ,
-  PRIMARY KEY (`user_id`) ,
-  UNIQUE INDEX `username_clean` (`username_clean` ASC) ,
-  INDEX `user_birthday` (`user_birthday` ASC) ,
-  INDEX `user_email_hash` (`user_email_hash` ASC) ,
-  INDEX `user_type` (`user_type` ASC) )
 ENGINE = MyISAM
-AUTO_INCREMENT = 53
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
 
@@ -157,11 +83,13 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `phpbb`.`phpbb_users_has_conferencia`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `phpbb`.`phpbb_users_has_conferencia` ;
+
 CREATE  TABLE IF NOT EXISTS `phpbb`.`phpbb_users_has_conferencia` (
   `phpbb_users_user_id` MEDIUMINT(8) UNSIGNED NOT NULL ,
   `Conferencia_idConferencia` INT NOT NULL ,
-  `token_asignado` TINYINT(1)  NOT NULL ,
-  `rol_relacion` VARCHAR(1) NOT NULL ,
+  `token_asignado` TINYINT(1)  NOT NULL COMMENT 'Si la relacion tiene un rol de votante, indica si se ha asignado un token al votante o no.  Esto para no dar màs de un token a un votante en una conferencia.\nSi el rol de la relacion es conferencista, este atributo debe se nulo o ignorado.\n' ,
+  `rol_relacion` ENUM('V','C') NOT NULL COMMENT 'El rol que toma en la relacion de usuario con conferencia. Un rol de votante o un rol de conferencista.\nSe puede manejar' ,
   PRIMARY KEY (`phpbb_users_user_id`, `Conferencia_idConferencia`) ,
   INDEX `fk_phpbb_users_has_Conferencia_phpbb_users` (`phpbb_users_user_id` ASC) ,
   INDEX `fk_phpbb_users_has_Conferencia_Conferencia1` (`Conferencia_idConferencia` ASC) ,
@@ -172,15 +100,9 @@ CREATE  TABLE IF NOT EXISTS `phpbb`.`phpbb_users_has_conferencia` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_phpbb_users_has_Conferencia_Conferencia1`
     FOREIGN KEY (`Conferencia_idConferencia` )
-    REFERENCES `mydb`.`conferencia` (`id_conferencia` )
+    REFERENCES `phpbb`.`conferencia` (`id_conferencia` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_bin;
-
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- project/includes/functions_caldo.php
+++ project/includes/functions_caldo.php
@@ -1,22 +1,91 @@
 <?php
-/*! @function submit_caldo
-    @abstract Crea un caldo para una conferencia en un foro específico 
-    @param forum_id int - El id del foro en el que se desea crear el caldo
-    @param criterios array - Un array de los id's de los criterios que incluye el caldo 
-    @param conferencia array - Un array compuesto por 'titulo' y 'fecha', que da información sobre la conferencia a la que hará referencia el caldo
-    @param Votantes array - Un array de los id's de los nombres de usuarios habilitados para votar
-    @param conferencistas array - Un array con los nombres de usuarios que actuaron como conferencisatas y que portanto pueden ver el resultado de sus caldos.
-    @result Boolean - Devuelve verdadero si la creación del caldo fue exitosa y falso en cualquier otro caso.
+
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+define('CRITERIOS_EVALUACION_TABLE','criterios_evaluacion');
+define('CONFERENCIAS_TABLE','conferencia');
+
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+
+/** 
+ * Crea un caldo para una conferencia en un foro específico
+ *  @param	int		$topic_id	El id del foro en el que se desea crear el caldo
+ *  @param	array	$criterios	Un array de los id's de los criterios que incluye el caldo 
+ *  @param	array	$conferencia	Un array compuesto por 'titulo' y 'fecha', que da información sobre la conferencia a la que hará referencia el caldo
+ *  @param	array	$votantes		Un array de los id's de los nombres de usuarios habilitados para votar
+ *  @param	array	$conferencistas	Un array con los nombres de usuarios que actuaron como conferencisatas y que portanto pueden ver el resultado de sus caldos.
  */
-function submit_caldo($forum_id,$criterios,$conferencia,$votantes,$conferencistas){
+function submit_caldo($topic_id,$criterios,$conferencia,$votantes,$conferencistas){
 	//Verifique que exista el foro identificado por $forum_id
+	$sql='SELECT COUNT(*) AS temas_count
+		FROM '. TOPICS_TABLE ."
+		WHERE topic_id=$db->sql_escape($topic_id)";
+	$db->sql_query($sql);
+	if(((int) $db->sql_fetchfield('temas_count'))==0){
+		//Existe el tema
+	}
+	else{
+		//No existe el tema
+	}
 	
 	//Verifique que existan los criterios identificados por los id's en el array $criterios
+	$criterios_sql = array();
+	foreach($criterios as $criterio){
+		array_push($criterios_sql,(int)$db->sql_escape($criterio)); //Impedir que haya id's de criterios maliciosos o no enteros
+	}
+	
+	$sql = 'SELECT COUNT(*) as criterios_count
+		FROM'. CRITERIOS_EVALUACION_TABLE.'
+    	WHERE ' . $db->sql_in_set('id_criterios_evaluacion', $criterios_sql);
+	$db->sql_query($sql);
+	if (((int) $db->sql_fetchfield('criterios_count'))==count($criterios_sql)){
+		//Existen todos los criterios
+	}
 	
 	//Cree la conferencia y almacene su id en la variable $conferencia_id
 	
+	$sql = 'INSERT INTO '.CONFERENCIAS_TABLE."(fecha_realizacion,tema,phpbb_forums_forum_id)
+		VALUES('".date('Y-m-d',$db->sql_escape($conferencia['fecha']))."','".$db->sql_escape($conferencia['titulo'])."',$db->sql_escape($topic_id)";
+	if($db->sql_query($sql)){//Si la inserción se realiza exitosamente
+		$conferencia_id=$db->sql_nextid();	
+	}
+	
+	
+	
 	//Verifique la existencia de los nombres de usuarios en el array votantes y en el array conferencistas
 	
-	//
+	//Cree la conferencia y sus relaciones con los criterios y foro(s)
+	
+	//Cree las relaciones de conferencistas con conferencias.
+	
+	//Obtenga las direcciones de correo electrónico de los votantes
+	
+	//Genere tantos tokens como votantes haya y envíe cada uno a un correo de votante
+	
+}
+/*! @function verify_tokens
+    @abstract Verifica la validez de los tokens en la tabla tokens a partir de su fecha de vencimiento.
+ */
+function verify_tokens(){
+	//Verificar token por token si la fecha de vencimiento ya pasó y borrar el token en caso de que se haya borrado
+}
+
+function submit_vote($conferencia_id,$token,$criterios){
+	//Verificar la validez del token ... verificar si el token es válido para la conferencia?
+	
+	//Multiplicar el valor actual en el puntaje de cada criterio relacionado con la conferencia, por el número de votos, para obtener el total de la suma de puntajes hasta el momento
+	
+	//Sumarle uno al número de votos en la conferencia
+	
+	//Sumar los nuevos puntajes a cada acumulado de puntaje de criterio y luego dividir este nuevo acumulado entre el nuevo nnùmero de votos  y guardar esta información en cada criterio 
 }
 ?>
