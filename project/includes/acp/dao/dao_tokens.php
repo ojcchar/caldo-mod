1da942ffa87dff57de57ff3ade016bf1958009ce
santiago1991 <santiago1991@localhost>
2010-07-05 17:53:53 -0500

    Archivos que olvid? agregar en el anterior commit

---
+++ project/includes/acp/dao/dao_tokens.php
@@ -0,0 +1,20 @@
+<?php
+class dao_tokens implements dao{
+	
+	public function crear($objeto){
+		throw new Exception('No implementado aun');
+	}
+	
+	public function actualizar($objeto){
+		throw new Exception('No implementado aun');
+	}
+	
+	public function eliminar($objeto){
+		throw new Exception('No implementado aun');
+	}
+	
+	public function consultar($consulta){
+		throw new Exception('No implementado aun');
+	}
+}
+?>
